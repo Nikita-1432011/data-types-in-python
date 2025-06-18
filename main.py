ac9c88048e1dffc180e4cf78d79358d39059a141
Nikita-1432011 <steppers.nikita3159@gmail.com>
2025-06-18 20:42:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 a= 7
 print("the type of a is",type(a))
 
-a= 7.34
-print("the type of a is",type(a))
+b= 7.34
+print("the type of a is",type(b))
 
-a= "8"
-print("the type of a is",type(a))
+c= "8"
+print("the type of a is",type(c))
 
-a= True
-print("the type of a is",type(a))
+d= True
+print("the type of a is",type(d))
+
+# type casting
+
+print("\n After type casting:")
+a= float(a)
+print("the type of a is:",type(a),"\n the value of a is:",a)
+
+b= int(b)
+print("the type of b is:",type(b),"\n the value of b is:",b)
+
+c= int(c)
+print("the type of c is:",type(c),"\n the value of c is:",c)
+
+n= str(b)
+print("the type of n is:",type(n),"\n the value of n is:",n)
